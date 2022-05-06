c41dd97996349b5211f61f02f834ba3b117c6f57
Sriram Karthik Akella <sk.akella7@gmail.com>
2022-05-06 10:52:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,33 @@
 # which email service do you use
 
 from email_validator import validate_email, EmailNotValidError
-
+import smtplib, ssl
 
 def validate(email):
-    try:
-        email = validate_email(email).email
-        valid = True
-    except EmailNotValidError as e:
-        # email is not valid, exception message is human-readable
-        print(str(e))
-        valid = False
+  try:
+    email = validate_email(email).email
+    valid = True
+  except EmailNotValidError as e: 
+    print(str(e))
+    valid = False
     
-    return valid
+  return valid
 
 def getService(email):
-    start = email.find("@") + len("@")
+  start = email.find("@") + len("@")
+  if email.count(".") == 1:
     end = email.find(".")
-    substring = email[start:end]
-    print(substring)
+  else:
+    end = email.find('.', email.find('.') + 1)
+  substring = email[start:end]
+  return substring
 
 def main():
-    email = "test@ing.com"
-    if validate(email) == True:
-        service = getService(email)
-        print(service)
-    else:
-        pass
+ 
+  email = "skakella7@gmail.com"
+  if validate(email) == True:
+    service = getService(email)
+    print(service)
+  else:
+      pass
+main()
